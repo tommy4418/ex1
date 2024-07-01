4fe876ab5700a6c0bcc9c5b6009b149796fada77
Tomas Rodriguez <tomas_rodriguez@epam.com>
2024-07-01 11:44:59 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: ExampleScreen(),
+    );
+  }
+}
+
+class ExampleScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exercise 1'),
+        foregroundColor: Colors.white,
+        backgroundColor: Color.fromARGB(255, 39, 58, 121),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 165, 186, 224),
+        ),
+        child: Center(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: Icon(Icons.map),
+                title: Text('Map'),
+                onTap: () {
+                  print('Map tapped');
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.photo_album),
+                title: Text('Album'),
+                onTap: () {
+                  print('Album tapped');
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.phone),
+                title: Text('Phone'),
+                onTap: () {
+                  print('Phone tapped');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Floating action button pressed');
+        },
+        child: Icon(Icons.navigation, color: Colors.white, size: 30.0),
+        backgroundColor: Color.fromARGB(255, 39, 58, 121),
+      ),
+    );
+  }
+}
