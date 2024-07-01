c01cb202b1ffa655bbd681665e01fe868e50d9c2
Tomas Rodriguez <tomas_rodriguez@epam.com>
2024-07-01 12:26:11 +0200

    changes in style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -27,34 +29,71 @@ class ExampleScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Exercise 1'),
+        title: Text(
+          'EXERCISE 1',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontFamily: GoogleFonts.lato().fontFamily,
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        
         foregroundColor: Colors.white,
-        backgroundColor: Color.fromARGB(255, 39, 58, 121),
+        backgroundColor: Color.fromARGB(255, 4, 12, 36),
       ),
       body: Container(
         decoration: BoxDecoration(
-          color: Color.fromARGB(255, 165, 186, 224),
+          color: Color.fromARGB(255, 5, 37, 97),
         ),
         child: Center(
           child: ListView(
             children: <Widget>[
               ListTile(
-                leading: Icon(Icons.map),
-                title: Text('Map'),
+                leading: Icon(Icons.map, color: Colors.white, size: 30.0),
+                title: Text(
+                  'Map',
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontFamily: GoogleFonts.lato().fontFamily,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24.0,
+                  ),
+                ),
                 onTap: () {
                   print('Map tapped');
                 },
               ),
               ListTile(
-                leading: Icon(Icons.photo_album),
-                title: Text('Album'),
+                leading:
+                    Icon(Icons.photo_album, color: Colors.white, size: 30.0),
+                title: Text(
+                  'Album',
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontFamily: GoogleFonts.lato().fontFamily,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24.0,
+                  ),
+                ),
                 onTap: () {
                   print('Album tapped');
                 },
               ),
               ListTile(
-                leading: Icon(Icons.phone),
-                title: Text('Phone'),
+                leading: Icon(Icons.phone, color: Colors.white, size: 30.0),
+                title: Text(
+                  'Phone',
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontFamily: GoogleFonts.lato().fontFamily,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24.0,
+                  ),
+                ),
                 onTap: () {
                   print('Phone tapped');
                 },
@@ -68,7 +107,7 @@ class ExampleScreen extends StatelessWidget {
           print('Floating action button pressed');
         },
         child: Icon(Icons.navigation, color: Colors.white, size: 30.0),
-        backgroundColor: Color.fromARGB(255, 39, 58, 121),
+        backgroundColor: Color.fromARGB(255, 197, 156, 43),
       ),
     );
   }
